efe3905d72256ca160a63dd06f29485044dca7c0
yuno-programing <iru.rui.ru@gmail.com>
2024-04-10 21:23:25 +0900

    AddError

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -20,7 +21,7 @@ class PostController extends Controller
     {
         return view('posts/create');
     }
-    public function store(Request $request, Post $post)
+    public function store(PostRequest $request, Post $post)
     {
        $input = $request['post'];
        $post->fill($input)->save();
