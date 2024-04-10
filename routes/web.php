a3b37440a231b09f4fadf1649550aece5ab7e295
yuno-programing <iru.rui.ru@gmail.com>
2024-04-10 16:52:14 +0900

    MadeBlogPostsScreen

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,7 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
